0b987efd01c6fe423ff3795a00a1c2dbabded9b5
KeyurJP <patel.keyur1123@gmail.com>
2024-08-06 19:34:03 +0530

    Rollback the last changes.

--- SilverlightContrib.Controls/GroupBox/GroupBox.cs
+++ SilverlightContrib.Controls/GroupBox/GroupBox.cs
@@ -16,8 +16,6 @@ namespace SilverlightContrib.Controls
         private RectangleGeometry HeaderRect;
         private ContentControl HeaderContainer;
 
-        private bool _loaded, _applyTemplateDone;
-
         /// <summary>
         /// Creates a new instance of the GroupBox control.
         /// </summary>
@@ -25,49 +23,6 @@ namespace SilverlightContrib.Controls
         {
             DefaultStyleKey = typeof(GroupBox);
             this.SizeChanged += GroupBox_SizeChanged;
-            Loaded += (s, a) => {
-                _loaded = true;
-                PostponeUpdateInitialBackground();
-            };
-        }
-
-        private void PostponeUpdateInitialBackground() {
-            if (_loaded && _applyTemplateDone)
-                Dispatcher.BeginInvoke(UpdateInitialBackground);
-        }
-
-        private static bool IsTransparentBrush(Brush brush) {
-            if (brush != null) {
-                if (brush is SolidColorBrush solid) {
-                    if (solid.Color != Colors.Transparent)
-                        return false;
-                } else 
-                    // background of a different Brush type, assume not transparent
-                    return false;
-            }
-
-            return true;
-        }
-
-        private void UpdateInitialBackground() {
-            if (!IsTransparentBrush(Background))
-                return;
-
-            var parent = VisualTreeHelper.GetParent(this);
-            Brush parentBrush = null;
-            while (parent != null && parentBrush == null) {
-                if (parent is Control control) {
-                    if (!IsTransparentBrush(control.Background))
-                        parentBrush = control.Background;
-                } else if (parent is Panel panel) {
-                    if (!IsTransparentBrush(panel.Background))
-                        parentBrush = panel.Background;
-                }
-                parent = VisualTreeHelper.GetParent(parent);
-            }
-
-            if (parentBrush != null)
-                Background = parentBrush;
         }
 
         /// <summary>
@@ -81,15 +36,12 @@ namespace SilverlightContrib.Controls
             HeaderRect = (RectangleGeometry)GetTemplateChild("HeaderRect");
             HeaderContainer = (ContentControl)GetTemplateChild("HeaderContainer");
             HeaderContainer.SizeChanged += HeaderContainer_SizeChanged;
-
-            _applyTemplateDone = true;
-            PostponeUpdateInitialBackground();
         }
 
         /// <summary>
         /// Header Dependency Property
         /// </summary>
-        public static readonly DependencyProperty HeaderProperty = 
+        public static readonly DependencyProperty HeaderProperty =
             DependencyProperty.Register("Header", typeof(object), typeof(GroupBox), null);
 
         /// <summary>
@@ -104,7 +56,7 @@ namespace SilverlightContrib.Controls
         /// <summary>
         /// HeaderTemplate dependency property.
         /// </summary>
-        public static readonly DependencyProperty HeaderTemplateProperty = 
+        public static readonly DependencyProperty HeaderTemplateProperty =
             DependencyProperty.Register("HeaderTemplate", typeof(DataTemplate), typeof(GroupBox), null);
 
         /// <summary>
@@ -124,6 +76,6 @@ namespace SilverlightContrib.Controls
         private void HeaderContainer_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             HeaderRect.Rect = new Rect(new Point(HeaderContainer.Margin.Left, 0), e.NewSize);
-        } 
+        }
     }
 }
